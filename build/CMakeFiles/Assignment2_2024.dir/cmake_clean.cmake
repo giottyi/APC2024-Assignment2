7c2aa2a25510285d60bb221dd807204742e6c31f
Francesco Giaiotti <giaiotti.francesco@gmail.com>
2025-01-10 12:40:52 +0100

    fixed compiler warnings

---
+++ build/CMakeFiles/Assignment2_2024.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "Assignment2_2024"
+  "Assignment2_2024.pdb"
+  "CMakeFiles/Assignment2_2024.dir/cnn.cpp.o"
+  "CMakeFiles/Assignment2_2024.dir/cnn.cpp.o.d"
+  "CMakeFiles/Assignment2_2024.dir/convolutional_layer.cpp.o"
+  "CMakeFiles/Assignment2_2024.dir/convolutional_layer.cpp.o.d"
+  "CMakeFiles/Assignment2_2024.dir/dataset.cpp.o"
+  "CMakeFiles/Assignment2_2024.dir/dataset.cpp.o.d"
+  "CMakeFiles/Assignment2_2024.dir/fc_layer.cpp.o"
+  "CMakeFiles/Assignment2_2024.dir/fc_layer.cpp.o.d"
+  "CMakeFiles/Assignment2_2024.dir/main.cpp.o"
+  "CMakeFiles/Assignment2_2024.dir/main.cpp.o.d"
+  "CMakeFiles/Assignment2_2024.dir/matrix.cpp.o"
+  "CMakeFiles/Assignment2_2024.dir/matrix.cpp.o.d"
+  "CMakeFiles/Assignment2_2024.dir/max_pooling_layer.cpp.o"
+  "CMakeFiles/Assignment2_2024.dir/max_pooling_layer.cpp.o.d"
+  "CMakeFiles/Assignment2_2024.dir/relu.cpp.o"
+  "CMakeFiles/Assignment2_2024.dir/relu.cpp.o.d"
+  "CMakeFiles/Assignment2_2024.dir/sigmoid.cpp.o"
+  "CMakeFiles/Assignment2_2024.dir/sigmoid.cpp.o.d"
+  "CMakeFiles/Assignment2_2024.dir/tensor_3d.cpp.o"
+  "CMakeFiles/Assignment2_2024.dir/tensor_3d.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment2_2024.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
